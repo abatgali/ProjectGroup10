57600bef0f678420c5e060048d57266bd4df1ad8
abatgali <abatgali@iu.edu>
2021-11-10 15:47:37 -0500

    edited the header file

--- includes/header.php
+++ includes/header.php
@@ -6,9 +6,22 @@
 
     </head>
     <body>
+        <div class="header">
+            <div id="nav">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Menu</a></li>
+                    <li><a href="">About Us</a></li>
+                </ul>
+            </div>
+            <h3>Chinese Bistro</h3>
+            <div id="loginAndCart">
+                <a href="">Log In</a>
+                <a href="">Cart</a>
+            </div>
+        </div>
 
-
-    <div>
-        <!--link to the damn menu if it works -->
-        <a href="listmenu.php">List Menu</a> ||
-    </div>
+        <div>
+            <!--link to the damn menu if it works -->
+            <a href="listmenu.php">List Menu</a> ||
+        </div>
