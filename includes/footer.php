0e5b39f95c106294607f96bcd62548bf9a7d8074
AlexWeber <aw118@iu.edu>
2021-11-10 15:24:52 -0500

    Made change from Alex's PC

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,11 @@
+<div>
+    <h3>Chinese Bistro</h3>
+    <p>440 Associate Avenue</p>
+    <p>Informatics, UG 46205</p>
+    <p><a href="tel: 317-278-9536">(317)-278-9536</a></p>
 
+    <p>Copyright &copy;  Lewi's Bistro. All Rights Reserved.</p>
+</div>
     </body>
 </html>
 
