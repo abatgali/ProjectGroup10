130d64f67e772cd9e5ffbe73d37bbc1c42188d26
AlexWeber <aw118@iu.edu>
2021-11-10 16:04:05 -0500

    Made change from Alex's PC

--- index.php
+++ index.php
@@ -9,6 +9,45 @@ require_once("includes/header.php");
 ?>
 <h1>Lewi's Chinese Bistro</h1>
 <p>Food is our specialty.</p>
+<body>
+<!--The "food is our specialty" block-->
+<div>
+    <h2>Food is our Specialty</h2>
 
+    <!--Paragraph text about the resturant-->
+    <p>Our creative, elevated food and beverages program combines
+    satisfying stamples with imaginative twist, from toasty brunches and happy
+    hours to family dinners, speical occasions and everything in between. Lewi's
+    Chinese Bistro has something for everyone.</p>
+
+    <!---Order online button-->
+    <input type="button"
+           onclick=""
+
+    <!---Make a reservation for lewis-->
+    <input type="button"
+           onclick=""
+
+
+</div>
+
+<!--Featured Items-->
+<div>
+    <h2>Featured Items</h2>
+
+</div>
+
+
+<!--Map Block-->
+<div>
+    <h2>Map</h2>
+
+
+    <p>Lorem ipusum dolor sit amet, conalsdjf;laksdjf;lkasdjf;laksdjf;lkasdjf;laksdjf</p>
+    <h4>Start Shopping</h4>
+
+</div>
+
+</body>
 <?php
 require_once ("includes/footer.php");
