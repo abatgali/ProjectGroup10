522f2b084e2b653e2c6a0c91c9bfec950d71baa9
abatgali <abatgali@iu.edu>
2021-11-11 14:51:12 -0500

    added search input in listmenu.php

--- listmenu.php
+++ listmenu.php
@@ -47,6 +47,9 @@ if(!$row){
             </ul>
         </nav>
         <h2>Menu Items</h2>
+        <div id="search">
+            <input type="text" value="Search...">
+        </div>
         <div class="menuItems">
             <div class="booklist">
                 <div class="col1">Product: </div>
