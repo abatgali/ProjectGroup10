35e416c0078363ee5cb4fe9dd52de30c7bf7f005
AlexWeber <aw118@iu.edu>
2021-11-11 14:09:29 -0500

    e

--- listmenu.php
+++ listmenu.php
@@ -6,11 +6,12 @@
 
 $page_title = "Menu";
 require 'includes/header.php';
+require 'includes/head.php';
 require_once('includes/database.php');
 //Use a select statement to retrieve information form the
 //men_items table
 
-$sql = "SELECT Item_id, Product_name,Category_id,Price FROM $tblMenu";
+$sql = "SELECT Item_id, Product_name,Category_id,Price,Description FROM $tblMenu";
 
 //to attempt a query execute
 $query = $conn->query($sql);
@@ -33,19 +34,17 @@ if(!$row){
 
 <h2>Menu Items</h2>
 <div>
-    <div>
-        <div>Item ID</div>
-        <div>Product Name</div>
-        <div>Category ID</div>
-        <div>Price</div>
+    <div class="booklist">
+        <div class="col1">Product: </div>
+        <div class="col2">Description</div>
+        <div class="col3">Price</div>
     </div>
     <!-- add PHP code here to list all menu items from the "menu" table -->
     <?php while($row = $query->fetch_assoc()){?>
         <div class="row">
-            <div class="col1"><?= $row['Item_id']?></div>
-            <div class="col2"><?= $row['Product_name']?></div>
-            <div class="col3"><?= $row['Category_id']?></div>
-            <div class="col4"><?= $row['Price']?></div>
+            <div class="col1"><?= $row['Product_name']?></div>
+            <div class="col2"><?= $row['Description']?></div>
+            <div class="col3"><?= $row['Price']?></div>
         </div>
     <?php }?>
 </div>
