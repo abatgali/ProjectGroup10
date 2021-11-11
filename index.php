9c2c51ebd4a9137e2dc0b8b97f28d1bf3313b1df
abatgali <abatgali@iu.edu>
2021-11-11 13:27:31 -0500

    copied James' feature items div into index file

--- index.php
+++ index.php
@@ -11,37 +11,59 @@ require_once("includes/header.php");
 <p>Food is our specialty.</p>
 <body>
 <!--The "food is our specialty" block-->
-<div>
     <h2>Food is our Specialty</h2>
 
-    <!--Paragraph text about the resturant-->
+    <!--Paragraph text about the restaurant-->
     <p>Our creative, elevated food and beverages program combines
-    satisfying stamples with imaginative twist, from toasty brunches and happy
-    hours to family dinners, speical occasions and everything in between. Lewi's
+    satisfying samples with imaginative twist, from toasty brunches and happy
+    hours to family dinners, special occasions and everything in between. Lewi's
     Chinese Bistro has something for everyone.</p>
 
     <!---Order online button-->
     <input type="button"
-           onclick=""
+           onclick="" value="Order Online">
 
     <!---Make a reservation for lewis-->
     <input type="button"
-           onclick=""
-
-
+           onclick="" value="Make a Reservation">
 </div>
 
 <!--Featured Items-->
-<div>
+<div class="featureItems">
     <h2>Featured Items</h2>
-
+        <div class="feature1">
+            <input type="image">
+            <p>Category Name</p>
+        </div>
+        <div class="feature2">
+            <input type="image">
+            <p>Category Name</p>
+        </div>
+        <div class="feature3">
+            <input type="image">
+            <p>Category Name</p>
+        </div>
+        <div class="feature4">
+            <input type="image">
+            <p>Category Name</p>
+        </div>
+        <div class="feature5">
+            <input type="image" >
+            <p>Category Name</p>
+        </div>
+        <div class="feature6">
+            <input type="image">
+            <p>Category Name</p>
+        </div>
 </div>
 
 
 <!--Map Block-->
 <div>
     <h2>Map</h2>
-
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3066.4258867174885!2d-86.17947384931252!3d39.774996802240466!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x886b5735461f008b%3A0xba2699b695cc371e!2sIUPUI!5e0!3m2!1sen!2sus!4v1636578375936!5m2!1sen!2sus"
+            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy">
+    </iframe>
 
     <p>Lorem ipusum dolor sit amet, conalsdjf;laksdjf;lkasdjf;laksdjf;lkasdjf;laksdjf</p>
     <h4>Start Shopping</h4>
