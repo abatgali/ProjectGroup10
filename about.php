2daa88a9cf17f9293c33e8884cc299e378bb97f5
IsaacLowe4 <ijlowe@iu.edu>
2021-12-02 00:03:48 -0500

    About Us completed

--- about.php
+++ about.php
@@ -12,31 +12,29 @@ require("includes/header.php");
 ?>
 
             <div class="aboutTop">
-                <h2>About Us</h2>
-                <p>Been in business for over 10 years and...</p>
+                <h2 class="aboutTitle">About Us</h2>
+                <p class="aboutUs">Louie's has been in the american chinese business for 10 years, and it takes quite a crew to continuously maintain the title of #1 American Chinese food hotpot for 10 years straight. But its because of our crew, our leadership, and most importantly our loyal customers that we can continue to maintain this spot. </p>
             </div>
             <br>
             <div class="aboutMid">
                 <article>
-                    <h2>Our Mission</h2>
-                    <h3>Here at Louie's, we are dedicated to an authentic recreation of the chinese cuisine. Every day at Louie's we dedicate ourselves to maximizing</h3>
+                    <h1 class="aboutTitle"> Our Mission</h1><br>
+                    <h3>Here at Louie's, we are dedicated to an authentic presentation of the chinese cuisine into the american palette. Quality, Consistency, and Integrity are
+                        the three most important factors that go into every dish you're served by us, and we can promise you that you'll be able to taste the fresh, locally sourced, and handmade quality that goes into every dish.
+                        Every part of our crew from our CEO Louie Zhu, to the cashier at your local restaurant pride themselves on meeting these standards, and its because of this love and caring environment that we can provide america with the best chinese they'll ever have!
+                    </h3>
                 </article>
-                <aside>
-                    <div class="aboutMidPic1">
-                        <img src="" alt="Place image here">
+
+                    <div class="aboutMidPics">
+                        <img id="aboutMidPic1" src="images/I0000dHTeoiOvkfg.jpg" alt="Place image here">
+                        <img id="aboutMidPic2" src="images/istockphoto-466666638-170667a.jpg" alt="Place image here">
+                        <img id="aboutMidPic3" src="images/portrait-happy-proud-asian-chef-smiling-camera-kitchen-successful-restaurant-culinary-business-concept-178738673.jpg" alt="Place image here">
                     </div>
-                    <div class="aboutMidPic2">
-                        <img src="" alt="Place image here">
-                    </div>
-                    <div class="aboutMidPic3">
-                        <img src="" alt="Place image here">
-                    </div>
-                </aside>
+
             </div>
             <br>
             <div class="aboutBottom">
-                <h2>Meet Our Leaders</h2>
-                <p>Insert some fancy words here or something.</p>
+                <h2 class="aboutTitle">Our Industry Defining Team</h2>
                 <div class="leaderPhoto">
                     <div class="leader">
                         <img class="leaderPictures" src="images/red%20ranger.jpg" alt="Leader photo">
--- cart.php
+++ cart.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 /**
  * Author: James Ritter
  * Date: $(DATE)
