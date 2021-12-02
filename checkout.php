56221912d34cc115c15edeab2516bba5e16535ff
abatgali <abatgali@iu.edu>
2021-12-02 00:36:36 -0500

    static checkout page done + refined empty cart page

--- checkout.php
+++ checkout.php
@@ -4,4 +4,19 @@
  * Date: 12/1/21
  * File: checkout.php
  * Description:
- */ ?>
+ */
+
+$title = "Checkout confirmation";
+require_once 'includes/header.php';
+
+?>
+<div id="message">
+    <h2>Confirmation</h2>
+    <h3>Order Placed</h3>
+    <img src="https://img.icons8.com/ios-filled/100/000000/clock--v2.gif"/>
+    <h3>Will be ready in 20 minutes.</h3>
+</div>
+
+<?php
+require_once 'includes/footer.php';
+?>
